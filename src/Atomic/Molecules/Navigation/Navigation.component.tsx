5a3c31dd2c9475f8f6fe7e879b5f8846ed5024ac
Kevin McGrady <kevinmcgrady47@gmail.com>
2021-07-12 00:20:22 +0100

    updated styles

--- src/Atomic/Molecules/Navigation/Navigation.component.tsx
+++ src/Atomic/Molecules/Navigation/Navigation.component.tsx
@@ -6,8 +6,12 @@ import { Urls } from '../../../utils/urls';
 export const Navigation: React.FC = () => (
   <div className='navigation'>
     <ul className='navigation-list'>
-      <Link to={Urls.mainNavigation.work}>Work</Link>
-      <Link to={Urls.mainNavigation.about}>About</Link>
+      <li>
+        <Link to={Urls.mainNavigation.work}>Work</Link>
+      </li>
+      <li>
+        <Link to={Urls.mainNavigation.about}>About</Link>
+      </li>
     </ul>
   </div>
 );
